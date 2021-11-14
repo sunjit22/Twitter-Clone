1fbd418ee7238a3a6d22e50731c5c9aff165788c
sunjit22 <63877211+sunjit22@users.noreply.github.com>
2021-11-14 16:56:05 -0500

    removing extra files

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import './vendors/bootstrap/css/bootstrap.min.css';
 import './vendors/bootstrap/bootstrap.min.css';
 import './vendors/fontawesome-pro/css/all.css';
 import {BrowserRouter, Route} from "react-router-dom";
-import HelloWorld from "./components/a7/a7.js";
-import Practice from "./components/a7/Practice";
 import Build from "./components/a7/Build";
 
 
--- src/components/a7/a7.js
+++ src/components/a7/a7.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import History from "./History";
 
 const a7 = () => {
     return(
